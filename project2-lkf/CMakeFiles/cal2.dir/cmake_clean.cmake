b48840f8e938488a3dbee61cbf04391323b90194
DJever23 <13262218151@163.com>
2019-12-27 19:50:03 +0800

    add lkf's project

---
+++ project2-lkf/CMakeFiles/cal2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cal2.dir/CameraCalibration.cpp.o"
+  "cal2.pdb"
+  "cal2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cal2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
